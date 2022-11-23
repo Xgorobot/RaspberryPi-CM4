1a3f15919ada40df0ab5e42e4ec42488a93022e8
jd3096-mpy <1091329318@qq.com>
2022-11-23 19:28:52 +0800

    color

--- color.py
+++ color.py
@@ -8,39 +8,43 @@ import numpy as np
 import mediapipe as mp
 from numpy import linalg
 from xgolib import XGO
-dog = XGO(port='/dev/ttyAMA0',version="xgolite")
+g_dog = XGO(port='/dev/ttyAMA0',version="xgolite")
 
-
+red=(255,0,0)
+green=(0,255,0)
+blue=(0,0,255)
+yellow=(255,255,0)
 display = LCD_2inch.LCD_2inch()
 display.clear()
-splash = Image.new("RGB", (display.height, display.width ),"black")
+splash = Image.new("RGB", (display.height, display.width ),"black")   
 display.ShowImage(splash)
 button=Button()
 
-color_lower = np.array([156, 43, 46])
-color_upper = np.array([180, 255, 255])
-g_mode=1
-'''
-elif b.description == Name_widgets['Red'][g_ENABLE_CHINESE]:
 color_lower = np.array([0, 43, 46])
 color_upper = np.array([10, 255, 255])
-g_dog.action(0xff)
-g_mode = 1
-elif b.description == Name_widgets['Green'][g_ENABLE_CHINESE]:
-color_lower = np.array([35, 43, 46])
-color_upper = np.array([77, 255, 255])
-g_dog.action(0xff)
-g_mode = 1
-elif b.description == Name_widgets['Blue'][g_ENABLE_CHINESE]:
-color_lower=np.array([100, 43, 46])
-color_upper = np.array([124, 255, 255])
-g_dog.action(0xff)
-g_mode = 1
-elif b.description == Name_widgets['Yellow'][g_ENABLE_CHINESE]:
-color_lower = np.array([26, 43, 46])
-color_upper = np.array([34, 255, 255])
-g_dog.action(0xff)
-'''
+g_mode=1
+cm=1
+mode=1
+
+def change_color():
+    global color_lower,color_upper,mode
+    if mode==4:
+        mode=1
+    else:
+        mode+=1
+    if mode==1:
+        color_lower = np.array([0, 43, 46])
+        color_upper = np.array([10, 255, 255])
+    elif mode==2:
+        color_lower = np.array([35, 43, 46])
+        color_upper = np.array([77, 255, 255])
+    elif mode==3:
+        color_lower = np.array([35, 43, 46])
+        color_upper = np.array([77, 255, 255])
+    elif mode==4:
+        color_lower = np.array([35, 43, 46])
+        color_upper = np.array([77, 255, 255])
+
 
 #-----------------------COMMON INIT-----------------------
 font = cv2.FONT_HERSHEY_SIMPLEX 
@@ -50,10 +54,6 @@ cap.set(4,240)
 if(not cap.isOpened()):
     print("[camera.py:cam]:can't open this camera")
 
-    
-    
-    
-global color_lower, color_upper, g_mode
 t_start = time.time()
 fps = 0
 color_x = 0
@@ -75,16 +75,16 @@ while 1:
             if color_radius > 10:
                 # 将检测到的颜色标记出来  Mark the detected color
                 cv2.circle(frame,(int(color_x),int(color_y)),int(color_radius),(255,0,255),2)  
-                value_x = color_x - 320
-                value_y = color_y - 240
-                if value_x > 110:
-                    value_x = 110
-                elif value_x < -110:
-                    value_x = -110
-                if value_y > 150:
-                    value_y = 150
-                elif value_y < -150:
-                    value_y = -150
+                value_x = color_x - 160
+                value_y = color_y - 120
+                if value_x > 55:
+                    value_x = 55
+                elif value_x < -55:
+                    value_x = -55
+                if value_y > 75:
+                    value_y = 75
+                elif value_y < -75:
+                    value_y = -75
                 g_dog.attitude(['y','p'],[-value_x/10, value_y/10])
         else:
             color_x = 0
@@ -100,12 +100,22 @@ while 1:
     
     b,g,r = cv2.split(frame)
     img = cv2.merge((r,g,b))
+    if mode==1:
+        cv2.rectangle(img, (290, 10), (320, 40), red, -1)
+    elif mode==2:
+        cv2.rectangle(img, (290, 10), (320, 40), green, -1)
+    elif mode==3:
+        cv2.rectangle(img, (290, 10), (320, 40), blue, -1)
+    elif mode==4:
+        cv2.rectangle(img, (290, 10), (320, 40), yellow, -1)
     imgok = Image.fromarray(img)
     display.ShowImage(imgok)
     if (cv2.waitKey(1)) == ord('q'):
         break
     if button.press_b():
         break
+    if button.press_d():
+        change_color()
 
 cap.release()
 cv2.destroyAllWindows() 

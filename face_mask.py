b0cd20f212971f4cc8cce322929f3ec1e3ac9213
jd3096-mpy <1091329318@qq.com>
2022-12-05 11:12:41 +0800

    face_mask 2.0

--- face_mask.py
+++ face_mask.py
@@ -4,6 +4,8 @@ import spidev as SPI
 import LCD_2inch
 from PIL import Image,ImageDraw,ImageFont
 from key import Button
+import math
+import numpy as np
 from xgolib import XGO
 dog = XGO(port='/dev/ttyAMA0',version="xgolite")
 
@@ -21,47 +23,22 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
 
-# For static images:
-IMAGE_FILES = []
-drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-with mp_face_mesh.FaceMesh(
-    static_image_mode=True,
-    max_num_faces=1,
-    refine_landmarks=True,
-    min_detection_confidence=0.5) as face_mesh:
-  for idx, file in enumerate(IMAGE_FILES):
-    image = cv2.imread(file)
-    # Convert the BGR image to RGB before processing.
-    results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+def rotation_matrix_to_angles(rotation_matrix):
+    """
+    Calculate Euler angles from rotation matrix.
+    :param rotation_matrix: A 3*3 matrix with the following structure
+    [Cosz*Cosy  Cosz*Siny*Sinx - Sinz*Cosx  Cosz*Siny*Cosx + Sinz*Sinx]
+    [Sinz*Cosy  Sinz*Siny*Sinx + Sinz*Cosx  Sinz*Siny*Cosx - Cosz*Sinx]
+    [  -Siny             CosySinx                   Cosy*Cosx         ]
+    :return: Angles in degrees for each axis
+    """
+    x = math.atan2(rotation_matrix[2, 1], rotation_matrix[2, 2])
+    y = math.atan2(-rotation_matrix[2, 0], math.sqrt(rotation_matrix[0, 0] ** 2 +
+                                                     rotation_matrix[1, 0] ** 2))
+    z = math.atan2(rotation_matrix[1, 0], rotation_matrix[0, 0])
+    return np.array([x, y, z]) * 180. / math.pi
 
-    # Print and draw face mesh landmarks on the image.
-    if not results.multi_face_landmarks:
-      continue
-    annotated_image = image.copy()
-    for face_landmarks in results.multi_face_landmarks:
-      print('face_landmarks:', face_landmarks)
-      mp_drawing.draw_landmarks(
-          image=annotated_image,
-          landmark_list=face_landmarks,
-          connections=mp_face_mesh.FACEMESH_TESSELATION,
-          landmark_drawing_spec=None,
-          connection_drawing_spec=mp_drawing_styles
-          .get_default_face_mesh_tesselation_style())
-      mp_drawing.draw_landmarks(
-          image=annotated_image,
-          landmark_list=face_landmarks,
-          connections=mp_face_mesh.FACEMESH_CONTOURS,
-          landmark_drawing_spec=None,
-          connection_drawing_spec=mp_drawing_styles
-          .get_default_face_mesh_contours_style())
-      mp_drawing.draw_landmarks(
-          image=annotated_image,
-          landmark_list=face_landmarks,
-          connections=mp_face_mesh.FACEMESH_IRISES,
-          landmark_drawing_spec=None,
-          connection_drawing_spec=mp_drawing_styles
-          .get_default_face_mesh_iris_connections_style())
-    cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+
 
 # For webcam input:
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
@@ -74,6 +51,19 @@ with mp_face_mesh.FaceMesh(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as face_mesh:
   while cap.isOpened():
+    face_coordination_in_real_world = np.array([
+        [285, 528, 200],
+        [285, 371, 152],
+        [197, 574, 128],
+        [173, 425, 108],
+        [360, 574, 128],
+        [391, 425, 108]
+    ], dtype=np.float64)
+
+    h=240
+    w=320 
+    face_coordination_in_image = []
+    text=''
     success, image = cap.read()
     if not success:
       print("Ignoring empty camera frame.")
@@ -92,17 +82,6 @@ with mp_face_mesh.FaceMesh(
     direction=0
     if results.multi_face_landmarks:
       for face_landmarks in results.multi_face_landmarks:
-        point_list=face_landmarks.landmark
-        p1=point_list[0]
-        print(p1.x)
-        if p1.x>0.65:
-          direction=1
-          dog.attitude('y',-9)
-        elif p1.x<0.45:
-          direction=2
-          dog.attitude('y',9)
-        else:
-          dog.attitude('y',0)
         mp_drawing.draw_landmarks(
             image=image,
             landmark_list=face_landmarks,
@@ -124,16 +103,42 @@ with mp_face_mesh.FaceMesh(
             landmark_drawing_spec=None,
             connection_drawing_spec=mp_drawing_styles
             .get_default_face_mesh_iris_connections_style())
+            
+        for idx, lm in enumerate(face_landmarks.landmark):
+          if idx in [1, 9, 57, 130, 287, 359]:
+            x, y = int(lm.x * w), int(lm.y * h)
+            face_coordination_in_image.append([x, y])
+        face_coordination_in_image = np.array(face_coordination_in_image,dtype=np.float64)
+        # The camera matrix
+        focal_length = 1 * w
+        cam_matrix = np.array([[focal_length, 0, w / 2],
+                               [0, focal_length, h / 2],
+                               [0, 0, 1]])
+        # The Distance Matrix
+        dist_matrix = np.zeros((4, 1), dtype=np.float64)
+        success, rotation_vec, transition_vec = cv2.solvePnP(face_coordination_in_real_world, face_coordination_in_image,cam_matrix, dist_matrix)
+          # Use Rodrigues function to convert rotation vector to matrix
+        rotation_matrix, jacobian = cv2.Rodrigues(rotation_vec)
+        result = rotation_matrix_to_angles(rotation_matrix)
+        print(result)
+        pitch=result[0]
+        yaw=result[1]
+        roll=result[2]
+        dog.attitude(['p','y','r'],[int(-pitch/40*20),int(-yaw/60*15),int(-roll/40*15)])
+        
     else:
-      dog.attitude('y',0)
+      dog.reset()
     # Flip the image horizontally for a selfie-view display.
     b,g,r = cv2.split(image)
     image = cv2.merge((r,g,b))
     image = cv2.flip(image, 1)
-    if direction==1:
-      cv2.putText(image, 'LEFT', (10, 30), font, 1, (255, 255, 255), 2)
-    elif direction==2:
-      cv2.putText(image, 'RIGHT', (10, 30), font, 1, (255, 255, 255), 2)
+    try:
+      for i, info in enumerate(zip(('pitch', 'yaw', 'roll'), result)):
+        k, v = info
+        text = f'{k}: {int(v)}'
+        cv2.putText(image, text, (20, i*30 + 20),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (200, 0, 200), 2)
+    except:
+      pass
     imgok = Image.fromarray(image)
     display.ShowImage(imgok)
     #cv2.imshow('MediaPipe Face Mesh', cv2.flip(image, 1))
@@ -142,3 +147,4 @@ with mp_face_mesh.FaceMesh(
     if button.press_b():
       break
 cap.release()
+dog.reset()
